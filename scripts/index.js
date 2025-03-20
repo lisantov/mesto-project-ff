2779bcd4a97bd296b785e460dc16b547bb886f16
lisantov <lisantov@yandex.ru>
2025-03-20 11:33:22 +0700

    Ez v0.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
+
 function createCard(cardInfo, deleteCard) {
-    const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     
-    const cardsContainer = document.querySelector('.places__list');
-
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
     const cardDeleteButton = cardElement.querySelector('.card__delete-button');
@@ -13,16 +13,11 @@ function createCard(cardInfo, deleteCard) {
 
     cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));
 
-    return cardsContainer.append(cardElement);
+    return cardElement;
 }
 
 function deleteCard(item) {
     item.remove();
 }
 
-createCard({name: "Архыз", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"}, deleteCard);
-createCard({name: "Челябинская область", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg"}, deleteCard);
-createCard({name: "Иваново", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg"}, deleteCard);
-createCard({name: "Камчатка", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg"}, deleteCard);
-createCard({name: "Холмогорский район", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg"}, deleteCard);
-createCard({name: "Байкал", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg"}, deleteCard);
+initialCards.forEach(item => cardsContainer.append(createCard(item, deleteCard)));
