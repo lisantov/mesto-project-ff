c807b575792b131cdcaee93ce7beaa091c25f183
lisantov <lisantov@yandex.ru>
2025-04-11 21:56:05 +0700

    Reworking listeners

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,27 +9,42 @@ const elements = {
     editPopup: document.querySelector('.popup_type_edit'),
     addCardPopup: document.querySelector('.popup_type_new-card'),
     imagePopup: document.querySelector('.popup_type_image'),
-    profileName: document.querySelector('.profile__title'),
-    profileDesc: document.querySelector('.profile__description'),
     editButton: document.querySelector('.profile__edit-button'),
     addButton: document.querySelector('.profile__add-button')
 };
 
+/* СЛУШАТЕЛИ КНОПОК ОТКРЫТИЯ ПОПАПОВ */
+elements.editButton.addEventListener('click', () => {
+    elements.editPopup.addEventListener('click', (evt) => {
+        if(evt.target.classList.contains('popup__close') ||
+            evt.target.classList.contains('popup')) {
+                closeModal(elements.editPopup);
+            }
+    });
+    handleEditProfile();
+});
+elements.addButton.addEventListener('click', () => {
+    elements.addCardPopup.addEventListener('click', (evt) => {
+        if(evt.target.classList.contains('popup__close') ||
+            evt.target.classList.contains('popup')) {
+                closeModal(elements.addCardPopup);
+            }
+        });
+    handleAddCard();
+});
+
+/* СЛУШАТЕЛЬ ЗАКРЫТИЯ ПОПАПА С КАРТИНКОЙ */
+elements.imagePopup.addEventListener('click', (evt) => {
+    if(evt.target.classList.contains('popup__close') ||
+    evt.target.classList.contains('popup')) {
+        closeModal(elements.imagePopup);
+    }
+});
+
 /* ДОБАВЛЕНИЕ КАРТОЧЕК "ПО-УМОЛЧАНИЮ" */
 initialCards.forEach(item => elements.cardsContainer.append(createCard(item, deleteCard, likeHandler, imageHandler)));
 
-/* ФУНКЦИОНАЛЬНОСТЬ УВЕЛИЧЕНИЯ ИЗОБРАЖЕНИЯ КАРТОЧКИ */
-function imageHandler(cardInfo) {
-    const image = elements.imagePopup.querySelector('.popup__image');
-    const caption = elements.imagePopup.querySelector('.popup__caption');
-
-    image.src = cardInfo.link;
-    image.alt = cardInfo.name;
-    caption.textContent = cardInfo.name;
-    
-    openModal(elements.imagePopup);
-}
-/* ФУНКЦИОНАЛЬНОСТЬ ПОПА РЕДАКТИРОВАНИЯ ПРОФИЛЯ */
+/* ФУНКЦИОНАЛЬНОСТЬ ПОПАПА РЕДАКТИРОВАНИЯ ПРОФИЛЯ */
 function handleEditProfile() {
     const form = document.forms['edit-profile'];
     const nameInput = form.elements.name;
@@ -41,30 +56,25 @@ function handleEditProfile() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileDesc.textContent;
 
+    form.removeEventListener('submit', submitHandler);
+    form.addEventListener('submit', submitHandler);
     /* HANDLER ОТПРАВКИ ФОРМЫ ДЛЯ РЕДАКТИРОВАНИЯ ПРОФИЛЯ  */
     function submitHandler(evt) {
         evt.preventDefault();
         profileName.textContent = nameInput.value;
         profileDesc.textContent = jobInput.value;
-
-        form.removeEventListener('submit', submitHandler);
+        document.removeEventListener('keydown', handleKeyClose);
         closeModal(elements.editPopup);
     }
 
-    form.addEventListener('submit', submitHandler);
-
-    /* HANDLER ЗАКРЫТИЯ ФОРМЫ */
-    function handleClose(evt) {
-        if(evt.target.classList.contains('popup__close') || 
-            evt.target.classList.contains('popup')) closeModal(elements.editPopup);
-        else if(evt.key === 'Escape') closeModal(elements.editPopup);
+    document.removeEventListener('keydown', handleKeyClose);
+    document.addEventListener('keydown', handleKeyClose);
+    /* ЗАКРЫТИЕ ПОПАПА ЧЕРЕЗ ESC */
+    function handleKeyClose(evt) {
+        if(evt.key === 'Escape') {
+            closeModal(elements.editPopup);
+        }        
     }
-    
-    elements.editPopup.removeEventListener('click', handleClose);
-    document.removeEventListener('keydown', handleClose);
-
-    elements.editPopup.addEventListener('click', handleClose);
-    document.addEventListener('keydown', handleClose);
 
     openModal(elements.editPopup);
 }
@@ -75,41 +85,53 @@ function handleAddCard() {
     
     form.reset();
 
-    const placeNameInput = form.elements['place-name'];
-    const sourceLinkInput = form.elements['link'];
-
+    form.removeEventListener('submit', submitHandler);
+    form.addEventListener('submit', submitHandler);
     /* HANDLER ОТПРАВКИ ФОРМЫ ДЛЯ ДОБАВЛЕНИЯ КАРТОЧКИ  */
     function submitHandler(evt) {
         evt.preventDefault();
+        const placeNameInput = form.elements['place-name'];
+        const sourceLinkInput = form.elements['link'];
         const cardInfo = {
             name: placeNameInput.value,
             link: sourceLinkInput.value
         };
 
         elements.cardsContainer.prepend(createCard(cardInfo, deleteCard, likeHandler, imageHandler));
+        document.removeEventListener('keydown', handleKeyClose);
 
-        form.removeEventListener('submit', submitHandler);
         closeModal(elements.addCardPopup);
     }
 
-    form.addEventListener('submit', submitHandler);
-
-    /* HANDLER ЗАКРЫТИЯ ФОРМЫ */
-    function handleClose(evt) {
-        if(evt.target.classList.contains('popup__close') || 
-            evt.target.classList.contains('popup')) closeModal(elements.addCardPopup);
-        else if(evt.key === 'Escape') closeModal(elements.addCardPopup);
+    document.removeEventListener('keydown', handleKeyClose);
+    document.addEventListener('keydown', handleKeyClose);
+    /* ЗАКРЫТИЕ ПОПАПА ЧЕРЕЗ ESC */
+    function handleKeyClose(evt) {
+        if(evt.key === 'Escape') {
+            closeModal(elements.addCardPopup);
+        }        
     }
-    
-    elements.addCardPopup.removeEventListener('click', handleClose);
-    document.removeEventListener('keydown', handleClose);
-
-    elements.addCardPopup.addEventListener('click', handleClose);
-    document.addEventListener('keydown', handleClose);
 
     openModal(elements.addCardPopup);
 }
 
-/* СЛУШАТЕЛИ КНОПОК ОТКРЫТИЯ ПОПАПОВ */
-elements.editButton.addEventListener('click', handleEditProfile);
-elements.addButton.addEventListener('click', handleAddCard);
+/* ФУНКЦИОНАЛЬНОСТЬ УВЕЛИЧЕНИЯ ИЗОБРАЖЕНИЯ КАРТОЧКИ */
+function imageHandler(cardInfo) {
+    const image = elements.imagePopup.querySelector('.popup__image');
+    const caption = elements.imagePopup.querySelector('.popup__caption');
+
+    image.src = cardInfo.link;
+    image.alt = cardInfo.name;
+    caption.textContent = cardInfo.name;
+
+    document.addEventListener('keydown', handleKeyClose);
+    /* ЗАКРЫТИЕ ПОПАПА ЧЕРЕЗ ESC */
+    function handleKeyClose(evt) {
+        if(evt.key === 'Escape') {
+            document.removeEventListener('keydown', handleKeyClose);
+            closeModal(elements.imagePopup);
+        }        
+    }
+    
+    openModal(elements.imagePopup);
+}
